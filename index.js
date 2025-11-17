88e4e2184923e6dc477e17f6f641286a431ea676
DiamondGaming <121042320+caw8269@users.noreply.github.com>
2025-11-17 14:11:57 -0600

    setup blank html setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("it works!!!")
+const express = require('express');
+const app = express();
+const { Server } = require('socket.io');
+const http = require('http');
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(port, () => {
+    console.log(`server is listening on port: ${port}`);
+})
